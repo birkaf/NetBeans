6994409bcebceebf2de5fbb8ad4ca8be0476a337
AlekseevNote <AlekseevNote@AlekseevNote-HP>
2017-08-14 18:59:04 +0300

    + статистический метод ::findAll

---
+++ App/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Description of User
+ *
+ * @author AlekseevNote
+ */
+namespace App\Models;
+use App\Db;
+
+
+class User {
+    public $email;
+    public $name;
+    
+    public static function findAll(){
+        $db = new Db();
+        return $db->query(
+                'SELECT * FROM users',
+                '\App\Models\User'
+                );
+    }
+    //put your code here
+}
--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 require __DIR__ . '/autoload.php';
+/*
 $db = new \App\Db();
-$data = $db->query('SELECT * FROM foo');
-var_dump($data);
+
+$data = $db->query(
+        'SELECT * FROM users',
+        'App\\Models\\User'
+); //\App\Models\User::class       
+ * 
+ */
+/* public
+$user = new \App\Models\User();
+$users = $user->findAll();
+ * 
+ */
+$users = \App\Models\User::findAll();
+echo '<pre>';
+var_dump($users);
+echo '</pre>';
 //$res = $db->execute('CREATE TABLE foo (id SERIAL)');
 //var_dump($res);
